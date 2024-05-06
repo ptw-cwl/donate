8e2aba5250c329ab3d57f1b5cbfbc07470f02fef
ptw-cwl <79617221+ptw-cwl@users.noreply.github.com>
2024-05-06 15:35:26 +0800

    Delete asstes directory

--- asstes/scripts.js
+++
@@ -1,8 +0,0 @@
-function downloadImage(imagePath) {
-    var link = document.createElement('a');
-    link.href = imagePath;
-    link.download = imagePath.split('/').pop();
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
