a8c99699b836b34c892314a5f34d699cbf81c8b4
cwl <3503127564@qq.com>
2024-05-10 17:42:16 +0800

    样式的优化

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,36 @@
+var paymentInfo = {
+    wechat: {
+        imagePath: "assets/img/wechat_payment_code.jpg",
+        downloadPath: "assets/img/wechat_payment_code.jpg",
+        buttonText: "微信支付"
+    },
+    alipay: {
+        imagePath: "assets/img/alipay_payment_code.jpg",
+        downloadPath: "assets/img/alipay_payment_code.jpg",
+        buttonText: "支付宝支付"
+    }
+};
+
+var paymentMethodsContainer = document.getElementById('payment-methods');
+for (var method in paymentInfo) {
+    if (paymentInfo.hasOwnProperty(method)) {
+        var button = document.createElement('button');
+        button.textContent = paymentInfo[method].buttonText;
+        button.onclick = (function(method) {
+            return function() {
+                togglePayment(method);
+            };
+        })(method);
+        paymentMethodsContainer.appendChild(button);
+    }
+}
+
+function togglePayment(method) {
+    document.getElementById('payment-image').src = paymentInfo[method].imagePath;
+    document.getElementById('download-link').setAttribute('onclick', "downloadImage('" + paymentInfo[method].downloadPath + "')");
+    document.getElementById('download-link').innerHTML = '<i class="fas fa-download download-icon"></i>点击下载' + paymentInfo[method].buttonText + '二维码';
+}
+
 function downloadImage(imagePath) {
     var link = document.createElement('a');
     link.href = imagePath;
